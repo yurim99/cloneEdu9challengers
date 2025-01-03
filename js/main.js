c293f508c7b844141b8f34d1c01d93f7b682560f
yurim <me_28@naver.com>
2025-01-03 16:54:56 +0900

    토글되도록 수정

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     const semicircleCtrlBtn = document.querySelector('.semicircle__ctrl-btn');
     const semicircleCtrlOn = document.querySelector('.semicircle-ctrl__on');
+    let isReversed = false;
     
     semicircleCtrlBtn.addEventListener('click', function() {
         semicircleCtrlBtn.classList.add('deco-hide')
@@ -12,19 +13,27 @@ window.addEventListener("DOMContentLoaded", (event) => {
         let arrowNewRotation = arrowCurRotation === "rotate(181deg)" ? "rotate(0deg)" : "rotate(181deg)";
         arrow.style.transform = arrowNewRotation;
 
-        const part01 = document.querySelector('.part01');
-        const part02 = document.querySelector('.part02');
-        const part03 = document.querySelector('.part03');
-        part01.classList.add('active');
+        const parts = document.querySelector('.parts');
+        const part = parts.children;
 
-        setTimeout(function() {
-            part02.classList.add('active');
-        },500);
+        const order = isReversed? [2, 1, 0] : [0, 1, 2];
 
-        setTimeout(function() {
-            part03.classList.add('active');
-        },1200)
-
+        if(!isReversed){
+            order.forEach((index, i) => {
+                setTimeout(function() {
+                    part[index].classList.add('show');
+                    part[index].classList.remove('hide');
+                }, i * 750);
+            });
+        }else {
+            order.forEach((index, i) => {
+                setTimeout(function() {
+                    part[index].classList.remove('show');
+                    part[index].classList.add('hide');
+                }, i * 750);
+            });
+        }
+         isReversed = !isReversed;
     });
 
 });
