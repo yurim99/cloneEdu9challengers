02599e8877cbad007517afcf763ecd0bb201b251
yurim <me_28@naver.com>
2025-01-14 10:14:33 +0900

    팝업수정완료

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ window.addEventListener("DOMContentLoaded", (event) => {
     const semicircleCtrlOn = document.querySelector('.semicircle-ctrl__on');
     let isReversed = false;
     let isClicking = false;
-    
-    semicircleCtrlBtn.addEventListener('click', function() {
-        if(isClicking) return;
+
+    semicircleCtrlBtn.addEventListener('click', function () {
+        if (isClicking) return;
         isClicking = true;
-        
+
         setTimeout(() => {
             isClicking = false;
         }, 2250);
@@ -15,12 +15,12 @@ window.addEventListener("DOMContentLoaded", (event) => {
         semicircleCtrlBtn.classList.add('deco-hide');
         semicircleCtrlBtn.classList.toggle('active');
         semicircleCtrlOn.classList.toggle('active');
-        
+
         const arrow = document.querySelector('.mini__semicircle-arrow');
         const arrowCurRotation = window.getComputedStyle(arrow).transform;
         const newRotation = (arrowCurRotation === 'matrix(1, 0, 0, 1, 0, 0)' || arrowCurRotation === 'none') ? 'rotate(181deg)' : 'rotate(0deg)';
         arrow.style.transform = newRotation;
-        
+
         const parts = document.querySelector('.parts');
         const part = Array.from(parts.children);
 
@@ -32,101 +32,101 @@ window.addEventListener("DOMContentLoaded", (event) => {
                 part[index].classList.toggle('hide', isReversed);
             }, i * 750);
         });
-        
+
         setTimeout(() => {
             isReversed = !isReversed;
         }, order.length * 750);
     });
-    
+
     const cardNewsSwriper = new Swiper('.card-news__swriper', {
-        navigation : {
-            prevEl:'.popup-swiper___btn.prev',
-            nextEl:'.popup-swiper___btn.next',
+        navigation: {
+            prevEl: '.popup-swiper___btn.prev',
+            nextEl: '.popup-swiper___btn.next',
         },
         speed: 500,
     });
 
     const cardNewData = {
         cardNew01: {
-            title:'교육개혁 9대과제',
+            title: '교육개혁 9대과제',
             images: [
-                { src: "./images/component/cardNews/cardnews_swiper01_01.png"},
-                { src: "./images/component/cardNews/cardnews_swiper01_02.png"},
-                { src: "./images/component/cardNews/cardnews_swiper01_03.png"},
-                { src: "./images/component/cardNews/cardnews_swiper01_04.png"},
-                { src: "./images/component/cardNews/cardnews_swiper01_05.png"},
-                { src: "./images/component/cardNews/cardnews_swiper01_06.png"},
-                { src: "./images/component/cardNews/cardnews_swiper01_07.png"},
-                { src: "./images/component/cardNews/cardnews_swiper01_08.png"},
-                { src: "./images/component/cardNews/cardnews_swiper01_09.png"},
+                { src: "./images/component/cardNews/cardnews_swiper01_01.png" },
+                { src: "./images/component/cardNews/cardnews_swiper01_02.png" },
+                { src: "./images/component/cardNews/cardnews_swiper01_03.png" },
+                { src: "./images/component/cardNews/cardnews_swiper01_04.png" },
+                { src: "./images/component/cardNews/cardnews_swiper01_05.png" },
+                { src: "./images/component/cardNews/cardnews_swiper01_06.png" },
+                { src: "./images/component/cardNews/cardnews_swiper01_07.png" },
+                { src: "./images/component/cardNews/cardnews_swiper01_08.png" },
+                { src: "./images/component/cardNews/cardnews_swiper01_09.png" },
             ]
         },
         cardNew02: {
-            title:'2025년 교육부 예산안',
+            title: '2025년 교육부 예산안',
             images: [
-                { src: "./images/component/cardNews/cardnews_swiper02_01.png"},
-                { src: "./images/component/cardNews/cardnews_swiper02_02.png"},
-                { src: "./images/component/cardNews/cardnews_swiper02_03.png"},
-                { src: "./images/component/cardNews/cardnews_swiper02_04.png"},
-                { src: "./images/component/cardNews/cardnews_swiper02_05.png"},
+                { src: "./images/component/cardNews/cardnews_swiper02_01.png" },
+                { src: "./images/component/cardNews/cardnews_swiper02_02.png" },
+                { src: "./images/component/cardNews/cardnews_swiper02_03.png" },
+                { src: "./images/component/cardNews/cardnews_swiper02_04.png" },
+                { src: "./images/component/cardNews/cardnews_swiper02_05.png" },
             ],
         },
-        cardNew03 : {
+        cardNew03: {
             title: '고교학점제',
-            images : [
-                { src: "./images/component/cardNews/cardnews_swiper03_01.png"},
-                { src: "./images/component/cardNews/cardnews_swiper03_02.png"},
-                { src: "./images/component/cardNews/cardnews_swiper03_03.png"},
-                { src: "./images/component/cardNews/cardnews_swiper03_04.png"},
-                { src: "./images/component/cardNews/cardnews_swiper03_05.png"},
+            images: [
+                { src: "./images/component/cardNews/cardnews_swiper03_01.png" },
+                { src: "./images/component/cardNews/cardnews_swiper03_02.png" },
+                { src: "./images/component/cardNews/cardnews_swiper03_03.png" },
+                { src: "./images/component/cardNews/cardnews_swiper03_04.png" },
+                { src: "./images/component/cardNews/cardnews_swiper03_05.png" },
             ],
         },
         cardNew04: {
             title: 'AIDT(디지털 교과서)',
-            images :[
-                { src: "./images/component/cardNews/cardnews_swiper04_01.png"},
-                { src: "./images/component/cardNews/cardnews_swiper04_02.png"},
-                { src: "./images/component/cardNews/cardnews_swiper04_03.png"},
-                { src: "./images/component/cardNews/cardnews_swiper04_04.png"},
-                { src: "./images/component/cardNews/cardnews_swiper04_05.png"},
-                { src: "./images/component/cardNews/cardnews_swiper04_06.png"},
+            images: [
+                { src: "./images/component/cardNews/cardnews_swiper04_01.png" },
+                { src: "./images/component/cardNews/cardnews_swiper04_02.png" },
+                { src: "./images/component/cardNews/cardnews_swiper04_03.png" },
+                { src: "./images/component/cardNews/cardnews_swiper04_04.png" },
+                { src: "./images/component/cardNews/cardnews_swiper04_05.png" },
+                { src: "./images/component/cardNews/cardnews_swiper04_06.png" },
             ],
         },
         cardNew05: {
             title: '세계 최고 영유아 교육·보육',
             images: [
-                { src: "./images/component/cardNews/cardnews_swiper05_01.png"},
-                { src: "./images/component/cardNews/cardnews_swiper05_02.png"},
-                { src: "./images/component/cardNews/cardnews_swiper05_03.png"},
-                { src: "./images/component/cardNews/cardnews_swiper05_04.png"},
-                { src: "./images/component/cardNews/cardnews_swiper05_05.png"},
-                { src: "./images/component/cardNews/cardnews_swiper05_06.png"},
-                { src: "./images/component/cardNews/cardnews_swiper05_07.png"},
+                { src: "./images/component/cardNews/cardnews_swiper05_01.png" },
+                { src: "./images/component/cardNews/cardnews_swiper05_02.png" },
+                { src: "./images/component/cardNews/cardnews_swiper05_03.png" },
+                { src: "./images/component/cardNews/cardnews_swiper05_04.png" },
+                { src: "./images/component/cardNews/cardnews_swiper05_05.png" },
+                { src: "./images/component/cardNews/cardnews_swiper05_06.png" },
+                { src: "./images/component/cardNews/cardnews_swiper05_07.png" },
             ],
         },
         cardNew06: {
             title: '자립형 공립고 2.0',
             images: [
-                { src: "./images/component/cardNews/cardnews_swiper06_01.jpg"},
-                { src: "./images/component/cardNews/cardnews_swiper06_02.jpg"},
-                { src: "./images/component/cardNews/cardnews_swiper06_03.jpg"},
-                { src: "./images/component/cardNews/cardnews_swiper06_04.jpg"},
-                { src: "./images/component/cardNews/cardnews_swiper06_05.jpg"},
-                { src: "./images/component/cardNews/cardnews_swiper06_06.jpg"},
+                { src: "./images/component/cardNews/cardnews_swiper06_01.jpg" },
+                { src: "./images/component/cardNews/cardnews_swiper06_02.jpg" },
+                { src: "./images/component/cardNews/cardnews_swiper06_03.jpg" },
+                { src: "./images/component/cardNews/cardnews_swiper06_04.jpg" },
+                { src: "./images/component/cardNews/cardnews_swiper06_05.jpg" },
+                { src: "./images/component/cardNews/cardnews_swiper06_06.jpg" },
             ],
         },
         cardNew07: {
             title: '교육할동 보호제도',
             images: [
-                { src :"./images/component/cardNews/cardnews_swiper07_01.jpg"},
-                { src :"./images/component/cardNews/cardnews_swiper07_02.jpg"},
-                { src :"./images/component/cardNews/cardnews_swiper07_03.jpg"},
-                { src :"./images/component/cardNews/cardnews_swiper07_04.jpg"},
-                { src :"./images/component/cardNews/cardnews_swiper07_05.jpg"},
-                { src :"./images/component/cardNews/cardnews_swiper07_06.jpg"},
-                { src :"./images/component/cardNews/cardnews_swiper07_07.jpg"},
-                { src :"./images/component/cardNews/cardnews_swiper07_08.jpg"},
-                { src :"./images/component/cardNews/cardnews_swiper07_09.jpg"},
+                { src: "./images/component/cardNews/cardnews_swiper07_01.jpg" },
+                { src: "./images/component/cardNews/cardnews_swiper07_02.jpg" },
+                { src: "./images/component/cardNews/cardnews_swiper07_03.jpg" },
+                { src: "./images/component/cardNews/cardnews_swiper07_04.jpg" },
+                { src: "./images/component/cardNews/cardnews_swiper07_05.jpg" },
+                { src: "./images/component/cardNews/cardnews_swiper07_06.jpg" },
+                { src: "./images/component/cardNews/cardnews_swiper07_07.jpg" },
+                { src: "./images/component/cardNews/cardnews_swiper07_08.jpg" },
+                { src: "./images/component/cardNews/cardnews_swiper07_09.jpg" },
             ]
         }
     }
@@ -134,15 +134,15 @@ window.addEventListener("DOMContentLoaded", (event) => {
     const cardNewsBtns = document.querySelectorAll('.card-news-pop__btn');
     const cardNewsPopup = document.querySelector('.card-news__popup');
     cardNewsBtns.forEach(cardNewBtn => {
-        cardNewBtn.addEventListener('click', function() {
+        cardNewBtn.addEventListener('click', function () {
             const cardNewsPopupTit = document.getElementById('cardNewsPopupTit');
             const cardNewsBtnId = cardNewBtn.id;
             cardNewsPopupTit.textContent = cardNewData[cardNewsBtnId].title;
-            
+
             const cardNewSwiperWrapper = document.querySelector('.card-news__swriper .swiper-wrapper');
             cardNewSwiperWrapper.innerHTML = '';
 
-            cardNewData[cardNewsBtnId].images.forEach( image => {
+            cardNewData[cardNewsBtnId].images.forEach(image => {
                 const cardNewSlide = document.createElement('div');
                 cardNewSlide.className = 'swiper-slide';
                 cardNewSlide.innerHTML = `<img class="popup__img" src="${image.src}" alt="">`
@@ -156,26 +156,75 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
     const youtubeData = {
         youtube01: {
-            link:`<iframe width="560" height="315" src="https://www.youtube.com/embed/XFXudlMnE_4?si=J5_0427C14G_Jujg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+            midea: [
+                { type: "video", src: 'https://www.youtube.com/embed/XFXudlMnE_4?si=J5_0427C14G_Jujg' },
+            ],
         },
         youtube02: {
-            link:``
+            midea: [
+                { type: "video", src: 'https://www.youtube.com/embed/61fVEbQjmSI?si=oXtMK8VX5A5srQ7V' },
+            ], 
+        },
+        youtube03: {
+            midea: [
+                { type: "video", src: 'https://www.youtube.com/embed/bZsyWEJFlQo?si=SnRddh539oTA4TzO' },
+            ], 
+        },
+        youtube04: {
+            midea: [
+                { type: "video", src: 'https://www.youtube.com/embed/tiGKOaeKwto?si=epJUoWg5H5MG2h5v' },
+            ], 
+        },
+        youtube05: {
+            midea: [
+                { type: "video", src: 'https://www.youtube.com/embed/yG7JrRtFY08?si=f6B9Cxa2AO2pAjy9' },
+            ], 
+        },
+        youtube06: {
+            midea: [
+                { type: "video", src: 'https://www.youtube.com/embed/-ZUoWn6uvlc?si=E2w2dTyL3U1TPfNJ' },
+            ], 
+        },
+        youtube07: {
+            midea: [
+                { type: "video", src: 'https://www.youtube.com/embed/oSP5lWYgRzg?si=QK_jePeyCltZ0OJW' },
+            ], 
+        },
+        youtube08: {
+            midea: [
+                { type: "video", src: 'https://www.youtube.com/embed/YxKuJqPTAl4?si=0_9lHKEftzhYq5ds' },
+            ], 
+        },
+        youtube09: {
+            midea: [
+                { type: "video", src: 'https://www.youtube.com/embed/TXnM-qllQpw?si=BsinorufYXvM-77L' },
+            ], 
         },
     }
 
     const youtubeBtns = document.querySelectorAll('.youtube-pop__btn');
     const youtubePopup = document.querySelector('.youtube__popup');
+    const videoStage = document.querySelector('.video-stage');
+
     youtubeBtns.forEach(youtubeBtn => {
-        youtubeBtn.addEventListener('click', function() {
+        youtubeBtn.addEventListener('click', function () {
             const youtubeBtnId = youtubeBtn.id;
-            const youtubeBody = document.querySelector('.youtube__popup .popup__body');
-            
-            youtubeBody.innerHTML = '';
-            youtubeData[youtubeBtnId].links.forEach( link => {
-                const youtube = document.createElement('div');
-                youtube.className = 'youtube';
-                youtube.innerHTML = `${link}`
-            });
+
+            videoStage.innerHTML = '';
+
+            const selectedMedia = youtubeData[youtubeBtnId].midea[0];
+
+            if (selectedMedia && selectedMedia.type === "video") {
+                const iframe = document.createElement('iframe');
+                iframe.src = `${selectedMedia.src}&autoplay=1&mute=1`;
+                iframe.width = "560";
+                iframe.height = "315";
+                iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+                iframe.allowFullscreen = true;
+                iframe.classList.add('video');
+    
+                videoStage.appendChild(iframe);
+            }
 
             youtubePopup.classList.add('active');
         })
